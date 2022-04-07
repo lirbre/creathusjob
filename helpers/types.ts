4697567a8e3b7eff418c16f776f9bdb6a1e3304a
Breno Lira <lira_breno@hotmail.com>
2022-04-07 11:57:04 -0300

    feat: create a API call and add types to it

---
+++ helpers/types.ts
@@ -0,0 +1,16 @@
+export interface InfoProps {
+    track: string
+    title: string
+    startTime: Date
+    endTime: Date
+    events: EventsProp
+    speakers: string[]
+    campus: string
+}
+
+interface EventsProp {
+    title: string
+    startTime: Date
+    endTime: Date
+    certification?: Boolean
+}
