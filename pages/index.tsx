6a65363e6baf9e0e432c3f3cc7af578485b58634
Breno Lira <lira_breno@hotmail.com>
2022-05-04 10:54:52 -0300

    feat: useMemo optimization

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,47 @@
-import { Card , EventCard, Nav } from '@/components'
+import { EventCard, Nav } from '@/components'
 import { InfoProps } from '@/helpers/types'
-import { useTheme } from 'next-themes'
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 
 const Home = () => {
-  const { resolvedTheme, setTheme } = useTheme()
   const [info, setInfo] = useState<InfoProps[]>([])
   const [maxCards, setMaxCards] = useState<number>(4)
 
-  const getInformation = () => {
+  const getInformation = 
     useCallback(() => {
       fetch(
         'https://gist.githubusercontent.com/creathusjobs/3c53322e8dc8c3b524cfb2623c097cc5/raw/adc988f34c4fb1a0fe12035b4b030259776115ea/response.json'
       ).then((r) => r.json())
       .then((r) => {
         setInfo([...r])
-        console.log(r)
+        // console.log(r)
 
         return r
       })
     }, [])
-  }
 
   useEffect(() => {
     getInformation()
   }, [])
+
+  const memoMap = useMemo(() => {
+    const mapInfo = info.map((info, i: number) => 
+      i < maxCards && 
+        <div key={`${i}-${info.campus}`}>
+          <EventCard 
+            campus={info.campus}
+            endTime={info.endTime}
+            startTime={info.startTime}
+            events={info.events}
+            speakers={info.speakers}
+            title={info.title}
+            track={info.track}
+            detailsURL={info.detailsURL}
+          />
+        </div>
+    )
+
+  return mapInfo
+  }, [info])
 
   return (
     <div>
@@ -32,22 +49,7 @@ const Home = () => {
       <div className='max-w-7xl mx-auto pt-8 mt-20'>
         <h3 className='px-8 sm:text-left text-center'>Cursos</h3>
         <div className="container lg:mt-10 text-center max-w-7xl grid lg:grid-cols-4 md:grid-cols-3 lg:gap-8 sm:gap-4 gap-2 transition-all duration-500">
-          {info.map((info, i: number) => 
-            i < maxCards ? 
-              <div key={i}>
-                <EventCard 
-                  campus={info.campus}
-                  endTime={info.endTime}
-                  startTime={info.startTime}
-                  events={info.events}
-                  speakers={info.speakers}
-                  title={info.title}
-                  track={info.track}
-                  detailsURL={info.detailsURL}
-                />
-              </div>
-            : <></>
-          )}
+          {memoMap}
         </div>
         <div onClick={() => {
           maxCards === 4 ? setMaxCards(12) : setMaxCards(4)
